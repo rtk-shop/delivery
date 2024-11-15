febdafaed156a9c86ea3935a0886f4de7bac0578
fresonn <vladislavs319@gmail.com>
2024-11-15 14:11:42 +0200

    popular cities as in-memory data

--- internal/services/shared/popular-cities.go
+++ internal/services/shared/popular-cities.go
@@ -1,18 +1,5 @@
 package shared
 
-import (
-	"context"
-	"fmt"
-	"log"
-)
-
 func (s *service) PopularCities() ([]byte, error) {
-
-	res, err := s.cache.JSONGet(context.Background(), "popular_cities", ".").Result()
-	if err != nil {
-		log.Println(err)
-		return nil, fmt.Errorf("failed to get popular cities")
-	}
-
-	return []byte(res), nil
+	return s.populdarCities, nil
 }
--- internal/services/shared/service.go
+++ internal/services/shared/service.go
@@ -1,6 +1,9 @@
 package shared
 
 import (
+	"encoding/json"
+	"log"
+	"os"
 	"rtk/delivery/internal/config"
 
 	"github.com/redis/go-redis/v9"
@@ -11,13 +14,26 @@ type Service interface {
 }
 
 type service struct {
-	config *config.Config
-	cache  *redis.Client
+	config         *config.Config
+	cache          *redis.Client
+	populdarCities []byte
 }
 
 func New(config *config.Config, cache *redis.Client) Service {
+
+	data, err := os.ReadFile("json/popular_cities.json")
+	if err != nil {
+		log.Fatalln("failed to parse popular_cities.json", "error", err)
+	}
+
+	ok := json.Valid(data)
+	if !ok {
+		log.Fatalln("popular_cities.json not valid")
+	}
+
 	return &service{
-		config: config,
-		cache:  cache,
+		config:         config,
+		cache:          cache,
+		populdarCities: data,
 	}
 }
